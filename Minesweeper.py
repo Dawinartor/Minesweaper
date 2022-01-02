a94d0b8c8e938abcef5d92a1a21d8c96fa55c5ba
Mehmet Şahin Üçeş <mehmetsahin260@hotmail.com>
2022-01-02 23:50:01 +0200

    working on add_value

--- Minesweeper.py
+++ Minesweeper.py
@@ -5,19 +5,32 @@ class GameField:
     def __init__(self):
         self.field = np.zeros((16,16),dtype=object)
         self.fieldSize = self.field.size
-    
+        self.fieldSizeX = np.size(self.field,0)
+        self.fieldSizeY  = np.size(self.field,1)
+
+
     def placeZeros(self):
-        for x in range(0,np.size(self.field,0)):
-            for y in range(0,np.size(self.field,1)):
+        for x in range(0,self.fieldSizeX):
+            for y in range(0,self.fieldSizeY):
 
                 if  isinstance(self.field[x][y],Bomb) == True:
-                    pass
-                    #addValue(x,y)
+                    continue
 
                 self.field[x][y] = Number(x,y)
 
     def addValue(self,x,y):
-        pass
+
+        # for x in range(0,self.fieldSizeX):
+        #     for y in range(0,self.fieldSizeY):
+        #         if  isinstance(self.field[x][y],Bomb) == True:
+        #             self.field[x+1][y].increaseNumber()
+        #             self.field[x][y+1].increaseNumber()
+        #             self.field[x-1][y].increaseNumber()
+        #             self.field[x][y-1].increaseNumber()
+        #             self.field[x+1][y+1].increaseNumber()
+        #             self.field[x-1][y-1].increaseNumber()
+        #             self.field[x+1][y-1].increaseNumber()
+        #             self.field[x-1][y+1].increaseNumber()
 
 class Tile:
     def __init__(self,x,y):
