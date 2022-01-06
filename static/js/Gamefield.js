3ad85712c4228bc838f04750190a91ec8af149eb
David Melamed <david.melamed@web.de>
2022-01-06 18:07:13 +0200

    Trying to create visibile Tiles in Gamefield

--- static/js/Gamefield.js
+++ static/js/Gamefield.js
@@ -7,6 +7,13 @@ var gamefield = document.getElementById('gamefield');
  * @param {JSON} backEndJSON - Back-End JOSN object of the created game
  */
 function buildGamefield(backEndJSON) {
+    // create divs which represets Tiles
+    var allTiles = document.createElement('div');
+    var testText = document.createTextNode('I am a TEST');
+
+    allTiles.appendChild(testText);
     
+    // allTiles.classList.add('Tile')
+    gamefield.appendChild(allTiles);
 
 }
