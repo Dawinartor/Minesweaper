1403e3741d3c76c737dff6a82306d2715d981ef4
David Melamed <david.melamed@web.de>
2022-01-06 17:34:42 +0200

    Added Flask structure for Gamefield. CSS and JS in seperate folders.

--- static/js/Gamefield.js
+++ static/js/Gamefield.js
@@ -1,2 +1,12 @@
 var gamefield = document.getElementById('gamefield');
 
+
+/**
+ * Initinilize the visual part of the gamefield and adds to HTML document
+ * 
+ * @param {JSON} backEndJSON - Back-End JOSN object of the created game
+ */
+function buildGamefield(backEndJSON) {
+    
+
+}
