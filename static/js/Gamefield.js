6fb1e3c16c78d2a1400f87bc5bea738cfb257f85
David Melamed <david.melamed@web.de>
2022-01-06 19:06:02 +0200

    Added visualisation

--- static/js/Gamefield.js
+++ static/js/Gamefield.js
@@ -1,19 +1,36 @@
 var gamefield = document.getElementById('gamefield');
+//TODO: Add test backEndJSON
 
+/**
+ * Preprocessing the JSON gameobject to get all values to work with
+ * 
+ * @param {JSON} backEndJSON - Back-End JOSN object 
+ */
+function preProcessingGameobject(backEndJSON) {
+
+}
+
+//TODO: Add checking if gameobject arrived in correct form. -> Typescript
 
 /**
  * Initinilize the visual part of the gamefield and adds to HTML document
  * 
- * @param {JSON} backEndJSON - Back-End JOSN object of the created game
+ * @param {JSON} gameObject - Back-End JOSN object of the created game
  */
-function buildGamefield(backEndJSON) {
+function buildGamefield(gameObject) {
     // create divs which represets Tiles
-    var allTiles = document.createElement('div');
-    var testText = document.createTextNode('I am a TEST');
+    var tileList = [256]; //gameObject.__fieldSize = 256
 
-    allTiles.appendChild(testText);
-    
-    // allTiles.classList.add('Tile')
-    gamefield.appendChild(allTiles);
+    for (let i = 0; i < tileList.length; i++) {
+
+        var newTile = document.createElement('div');
+        newTile.className = 'tile';
+        gamefield.appendChild(newTile);
+
+    }
+
+    console.log(tileList[0]);
 
 }
+
+buildGamefield(null);
