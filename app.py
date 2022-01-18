07416af3133cd301b0e52b368cc6c66eb1d0c9a3
Mehmet Şahin Üçeş <mehmetsahin260@hotmail.com>
2022-01-18 22:16:18 +0200

    sending json and receiving is done

--- app.py
+++ app.py
@@ -1,42 +1,47 @@
 from logging import debug
+from tkinter import N
 from flask import Flask, render_template, redirect, request
 from flask import json as fjson
 from Minesweeper import Gamefield
 from flask_socketio import SocketIO,emit
 #! don't create game with starting the server
 
-
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = b'\x0f\xf6j\x07E\xb9an\x9d4\x19\xa0'
-socketio = SocketIO(app) # replaces the app.run() command
-
-newGame = Gamefield()
+socketio = SocketIO(app) # replaces the app.run() command #use logger=True, engineio_logger=True for debugging
 
 
+
+@socketio.on('connect')
 @app.route("/", methods=['GET', 'POST'])
-def index():                                # define data to give from back-end to front-end
-        # create Gamefield object
+def index():# define data to give from back-end to front-end\
+    global newGame
+    newGame = Gamefield()
+    # create Gamefield object
+    
+    message = newGame.toJSON()
+    socketio.emit('initialize',message)
     return render_template('Gamefield.html')
 
 
-#TODO: 
+#TODO: Send initialized gamefield through via websocket
 # Deploy JSON object in Flask for external uses in JS file
-@app.route("/startGame", methods=['GET', 'POST']) # access this with fetch api in js
-def testGame(): 
-    # GET request is default method 
-    message = newGame.toJSON() # return is string
+# @app.route("/startGame", methods=['GET', 'POST']) # access this with fetch api in js
+# def testGame(): 
+#     # GET request is default method 
+#     message = newGame.toJSON() # return is string
     
-    #writter(message)
-    return message
+#     #writter(message)
+#     return message
 
     
-@socketio.on('json')
+@socketio.on('click')
 def handle_message(json):
     tile = json['tile']
     newGame.isLightUpChanger(tile['x'],tile['y'])
     message = newGame.toJSON()
-    emit('json',message)
+    emit('newjson',message)
     
     #writter(message)
 
--- static/js/Gamefield.js
+++ static/js/Gamefield.js
@@ -4,8 +4,9 @@ var gameData; // data from back-end
 const socket = io();
 
 // this part recieves the modified json
-socket.on("json", (arg) => {
-    console.log(arg); 
+socket.on("newjson", (data) => {
+    gameData = JSON.parse(data);
+    blankDelete(gameData)
   });
 
 /**
@@ -13,21 +14,26 @@ socket.on("json", (arg) => {
  * 
  * @param {JSON} backEndJSON - Back-End JOSN object 
  */
-function preProcessingGame() {
-    // get game object from backend
-    fetch('/startGame')
-        .then(function(response) {
-            return response.text() // takes Response text value
-        })
-        .then(function(text) {
-            gameData = JSON.parse(text);
-            console.log(gameData);
-        })
-        .then(function() {
-            buildGamefield(gameData);
-        })
+// function preProcessingGame() {
+//     // get game object from backend
+//     fetch('/startGame')
+//         .then(function(response) {
+//             return response.text() // takes Response text value
+//         })
+//         .then(function(text) {
+//             gameData = JSON.parse(text);
+//             console.log(gameData);
+//         })
+//         .then(function() {
+//             buildGamefield(gameData);
+//         })
+// }
 
-}
+socket.on("initialize", (data) => {
+    gameData = JSON.parse(data);
+    blankDelete(gameData); 
+  });
+
 
 //TODO: Add checking if gameobject arrived in correct form. -> Typescript
 
@@ -57,6 +63,7 @@ function buildGamefield(gameObject) {
         gamefield.appendChild(newTile);
     }
 
+    
 
     // Function to log each click in console
     const logClickedTile = (event) => {
@@ -99,7 +106,7 @@ function buildGamefield(gameObject) {
 
     const sendClickedTile = (tileJSON) => {
 
-        socket.emit('json', tileJSON);
+        socket.emit('click', tileJSON);
     
     }
 
@@ -111,9 +118,11 @@ function buildGamefield(gameObject) {
 
         // log in system (console.log) each time which was clicked
         logClickedTile(clickedTile);
+        
         // collect clicked tile information & generate JSON
         let clickedTileJSON = getTileInformations(clickedTile);
         console.log(clickedTileJSON);
+        
         // send collected json
         sendClickedTile(clickedTileJSON);
     }
@@ -128,6 +137,107 @@ function buildGamefield(gameObject) {
 
 }
 
+
+
+function blankDelete(gameObject){
+    $('#gamefield').empty();
+
+    for (let i = 0; i < gameObject.Gamefield.fieldSize; i++) {
+        let newTile = document.createElement('div');
+        let tileP = document.createElement('p');
+        newTile.appendChild(tileP)
+
+        tileP.innerHTML = "0"
+        tileP.style.visibility = 'hidden'
+        
+        if (gameObject.Gamefield.field[i].isLightUp == true) {
+            newTile.className = 'blank';
+            
+            if (gameObject.Gamefield.field[i].number > 0 ){
+                tileP.style.visibility = 'visible'
+                tileP.innerHTML = String(gameObject.Gamefield.field[i].number);
+            }
+        }
+                   
+        else{
+            newTile.className = 'tile';
+            newTile.setAttribute('tileIndex', String(gameObject.Gamefield.field[i].index));
+            newTile.setAttribute('coordinate-x', String(gameObject.Gamefield.field[i].x)); // is row
+            newTile.setAttribute('coordinate-y', String(gameObject.Gamefield.field[i].y)); // is column
+        }
+        gamefield.appendChild(newTile);
+    }
+    // Function to log each click in console
+    const logClickedTile = (event) => {
+        // takes clicked tile in variable
+        let clickedTile = event.target;
+        
+        //~ for debugging
+    /*
+        console.log( 
+            clickedTile.getAttribute('tileIndex') + " /",
+            clickedTile.getAttribute('className') + " /",
+            clickedTile.getAttribute('coordinate-x') + " /",
+            clickedTile.getAttribute('coordinate-y') 
+        );
+    */
+    };
+
+    // Generates JSON from data
+    const createJSON = (valueX, valueY) => {
+        let returnJSON = {"tile": {
+            "x": valueX,
+            "y": valueY
+        }};
+        return returnJSON;
+    }
+
+    // collects information of clicked tile 
+    const getTileInformations = (event) => {
+        // takes clicked tile in variable
+        let clickedTile = event.target;
+        
+        let tileIndex = clickedTile.getAttribute('tileIndex');
+        let className = clickedTile.getAttribute('className');
+        let valueX = clickedTile.getAttribute('coordinate-x');
+        let valueY = clickedTile.getAttribute('coordinate-y');
+
+        // generates JSON with needed information
+        return createJSON(valueX, valueY);
+    }
+
+    const sendClickedTile = (tileJSON) => {
+
+        socket.emit('click', tileJSON);
+    
+    }
+
+    /**
+     * 
+     * @param {*} clickedTile 
+     */
+    const onClickTile = (clickedTile) => {
+
+        // log in system (console.log) each time which was clicked
+        logClickedTile(clickedTile);
+        
+        // collect clicked tile information & generate JSON
+        let clickedTileJSON = getTileInformations(clickedTile);
+        console.log(clickedTileJSON);
+        
+        // send collected json
+        sendClickedTile(clickedTileJSON);
+    }
+
+    // Get tile elements class from DOM
+    const tiles = document.querySelectorAll('.tile');
+
+    // Assign event listener with callback to every button:
+    tiles.forEach((tile) => {
+        tile.addEventListener("click", onClickTile);
+    });
+}
+
 //~ Communication between front-end & back-end
 
-preProcessingGame();
+// preProcessingGame();
